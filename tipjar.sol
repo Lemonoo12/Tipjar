4555387b68ca40df1d056723b8775292fa017f64
Lemonoo12 <gale55eye@gmail.com>
2026-02-03 11:19:21 +0700

    commit

---
+++ tipjar.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.31;
+
+contract tips {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // 1. Put fund in smart contract
+
+    function addtips() public payable {}
+
+    // 2. View balance
+
+    function viewtips() public view returns (uint) {
+        return address(this).balance;
+    }
+}
